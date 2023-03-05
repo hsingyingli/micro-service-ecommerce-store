6bada4ea11e2ce7092f7ecaf7cc52660b9b7370d
hsingyingli <xingyingli855@gmail.com>
2023-03-05 13:33:52 +0800

    re-design reusable component: ProductCart

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,22 +1,30 @@
+import { useImageUrl } from "@/hooks/useImageUrl";
 import { Product } from "@/store/providers/SellProvider";
 import Image from "next/image";
 import React from "react";
 
 interface Props {
   product: Product
+  children: React.ReactNode | null
 }
 
-const ProductCard: React.FC<Props> = ({ product }) => {
+const ProductCard: React.FC<Props> = ({ product, children }) => {
+  const url = useImageUrl(product.image_name)
   return (
-    <div className="w-full h-full ">
-      <div className="relative w-full aspect-[4/3] ">
+    <div className="rounded-md border-2 border-primary-500 overflow-hidden
+               hover:shadow-xl hover:shadow-primary-500 flex flex-row sm:flex-col
+               w-[calc(100vw_-_1rem)] aspect-[3/1]
+               sm:w-48 sm:aspect-[3/4]">
+      <div className="relative aspect-square h-full sm:h-auto w-auto sm:w-full ">
         <Image
-          fill alt={product.title} src={`data:image/${product.image_type};base64,${product.image_data}`} />
+          fill alt={product.title} src={url} />
       </div>
-      <div className="p-2">
-        <h1 className="text-lg text-secondary-600">{product.title} <span className="text-sm ml-2 text-secondary-500">({product.amount})</span></h1>
-        <p className="text-secondary-500 text-sm">NT$: {product.price}</p>
+      <div className="p-2 flex-grow">
+        <h1 className="text-2xl font-medium text-secondary-600">{product.title}</h1>
+        <p className="text-secondary-500 text-lg font-medium mt-2">NT$: {product.price}</p>
+        <p className="text-secondary-500 text-md font-medium">Remaining: {product.amount}</p>
       </div>
+      {children}
     </div>
   )
 }
