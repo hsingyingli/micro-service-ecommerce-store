bed93b310451a3b3df7d2c126090bc60265ccf06
hsingyingli <xingyingli855@gmail.com>
2023-03-07 08:47:07 +0800

    change order schema

--- order-service/config/db.sql
+++ order-service/config/db.sql
@@ -20,12 +20,28 @@ CREATE TABLE users (
 CREATE TABLE orders (
   id bigserial PRIMARY KEY NOT null,
   uid bigint NOT null,
-  pid bigint NOT null,
-  amount int not null,
   status varchar(10) not null DEFAULT 'WAIT',
   created_at TIMESTAMP not null DEFAULT(now()),
   updated_at TIMESTAMP not null DEFAULT(now())
 );
+
+CREATE TABLE order_items (
+  id bigserial PRIMARY KEY NOT null,
+  oid bigint NOT NULL,
+  pid bigint NOT NULL,
+  amount int not null
+  created_at TIMESTAMP not null DEFAULT(now()),
+  updated_at TIMESTAMP not null DEFAULT(now())
+);
+
+ALTER TABLE "order_items"
+ADD FOREIGN KEY ("oid")
+REFERENCES "orders" ("id")
+ON DELETE CASCADE;
+
+ALTER TABLE "order_items"
+ADD FOREIGN KEY ("pid")
+REFERENCES "products" ("id")
 
 ALTER TABLE "orders" 
 ADD FOREIGN KEY ("uid") 
