272c4815437e165abb0ce3b6366a6e3c91eef88c
hsingyingli <xingyingli855@gmail.com>
2023-03-08 14:05:45 +0800

    add amount and price

--- client/components/CartItemCard.tsx
+++ client/components/CartItemCard.tsx
@@ -10,12 +10,14 @@ interface Props {
 const CartItemCard: React.FC<Props> = ({ item }) => {
   const imageUrl = useImageUrl(item.image_name)
   return (
-    <div className="flex flex-row w-full h-full">
-      <div className="relative h-full aspect-square">
+    <div className="flex flex-row w-full h-full p-2">
+      <div className="relative h-full aspect-square overflow-hidden rounded-md">
         <Image src={imageUrl} alt={item.title} fill />
       </div>
-      <div className="px-2 ">
-        hello
+      <div className="px-2 flex-grow grid grid-cols-5 items-center">
+        <h1 className="text-xl col-start-1 col-end-4 truncate">{item.title}</h1>
+        <p className="col-start-4 col-end-5">{item.amount}</p>
+        <p className="col-start-5 col-end-6">${item.price}</p>
       </div>
     </div>
   )
